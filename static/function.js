0c7df79332cccb49901248a24d5bbd912240622d
leandroluizpereira <leandro.2306@hotmail.com>
2023-01-19 03:44:14 -0300

    n

---
+++ static/function.js
@@ -0,0 +1,48 @@
+
+  var test = document.getElementById("button");
+  
+  var n = document.getElementById("numberOne").value;
+  var nTwo = document.getElementById("numberTwo").value; 
+  var r = document.getElementById("result");
+  
+  
+
+test.onclick = function (){
+
+
+  var n = document.getElementById("numberOne").value;
+  var nTwo = document.getElementById("numberTwo").value; 
+  var r = document.getElementById("result");
+
+    
+    
+/*
+    for(var i = 0;i<10;i++){
+
+        
+
+        var calc = n * i;
+    
+       
+       r.innerHTML=" "+n+" X "+nTwo+" = "+calc;
+
+    
+    }
+    
+
+
+ 
+
+*/
+        
+
+var calc = n * nTwo;
+    
+       
+r.innerHTML=" "+n+" X "+nTwo+" = "+calc;
+
+}
+
+for(var i = 0;i<10;i++){
+console.log(n*i);
+}
